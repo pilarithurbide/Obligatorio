74eae77a4fd5f3b78e7ffd56e60c094d684b7289
pilarithurbide <68905395+pilarithurbide@users.noreply.github.com>
2020-08-12 20:49:47 -0300

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,6 +1,34 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+let url = "https://japdevdep.github.io/ecommerce-api/product/all.json"
 
-});
+var d1 = document.getElementById('contenido')
+
+function getJson(info){
+    return fetch(info)
+        .then( response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+            throw Error(response.statusText);
+            }
+        })
+}
+
+function enPantalla(obj){
+         for (let valor of obj) {
+            d1.innerHTML += 'Nombre: ' + valor.name + '<br/>' + 'Descripción: ' + valor.description + '<br/>' + 'Costo: USD ' + valor.cost + '<br/>' + '<br/>'
+         }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJson(url).then(function(data){
+        console.log(data);
+        var obj= data;
+        enPantalla(obj) 
+    })
+       
+    })
+
+;
